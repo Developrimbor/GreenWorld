b0785d0a07beaab57558085b1655d57b717977c4
Developrimbor <bymrglmz@gmail.com>
2025-06-09 23:47:43 +0300

    güncelle: TabLayout bileşeni. Klavye görünürlüğüne göre alt navigasyonun gösterimi eklendi. Ayrıca, klavye olayları için dinleyiciler tanımlandı.

--- RecycleApp/app/(tabs)/_layout.tsx
+++ RecycleApp/app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Platform, Keyboard } from 'react-native';
 import { router } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -9,17 +9,37 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import BottomNavigation from '../../components/BottomNavigation';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener(
+      Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow',
+      () => setIsKeyboardVisible(true)
+    );
+    const hideSubscription = Keyboard.addListener(
+      Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide',
+      () => setIsKeyboardVisible(false)
+    );
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="HomePage" />
-      <Stack.Screen name="PostDetail" />
-      <Stack.Screen name="UserProfile" />
-      <Stack.Screen name="ProfilePage" />
-      <Stack.Screen name="LikedPostsPage" />
-    </Stack>
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="HomePage" />
+        <Stack.Screen name="PostDetail" />
+        <Stack.Screen name="UserProfile" />
+        <Stack.Screen name="ProfilePage" />
+        <Stack.Screen name="LikedPostsPage" />
+      </Stack>
+      {!isKeyboardVisible && <BottomNavigation />}
+    </>
   );
 }
